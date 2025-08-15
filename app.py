0790961ffcc87f880f020f1273df670bf4b1af55
kanik <ka.nikhil.nka@gmail.com>
2025-08-15 19:39:28 +0530

    updated run command

--- app.py
+++ app.py
@@ -132,4 +132,7 @@ def analyze_document():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+    # app.run(host='0.0.0.0', port=port, debug=True)
