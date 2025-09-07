3c31ec74b40c00338b7e1669c122ede5bed29387
Didigam Nithin <nithindidigam@gmail.com>
2025-09-07 14:37:17 +0530

    Fix Railway deployment - PORT environment variable issue

--- app.py
+++ app.py
@@ -153,6 +153,4 @@ def analyze_document():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port)
-
-    # app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
