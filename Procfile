dc2b9f873e50125db13abb73f9cf5842965fe1c3
Didigam Nithin <nithindidigam@gmail.com>
2025-09-07 15:14:27 +0530

    Remove Railway configs and optimize for Render deployment

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn app:app --bind 0.0.0.0:${PORT:-8000} --timeout 120 --workers 1
--- app.py
+++ app.py
@@ -202,5 +202,5 @@ def summarize_document():
         }), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
